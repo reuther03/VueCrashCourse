068fc0bdff44e436d52d4ed6c6285a811569c3cc
michaldrozd <michaldrozd03@gmail.com>
2024-07-28 16:42:21 +0200

    Composition Api Long Form

--- src/App2.vue
+++ src/App2.vue
@@ -1,21 +1,21 @@
 <script>
-import { ref } from 'vue';
 export default {
-  setup(){
-    const name = ref('Joe Doe')
-    const status = ref('active')
-    const tasks = ref(['task1', 'task2', 'task3'])
-
-    const toggleStatus = () => {
-      if (status.value === "active") {
-        status.value = "inactive";
+  data() {
+    return {
+      name: 'Joe Doe',
+      status: 'active',
+      tasks: ['task1', 'task2', 'task3'],
+      link: 'https://www.google.com/'
+    };
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === "active") {
+        this.status = "inactive";
       } else {
-        status.value = "active";
+        this.status = "active";
       }
     }
-
-    return { name, status, tasks, toggleStatus };
-
   },
 };
 </script>
@@ -30,6 +30,8 @@ export default {
     <li v-for="task in tasks" :key="task">{{task}}</li>
   </ul>
 <!--  <a v-bind:href="link">google</a>-->
+  <a :href="link">google</a>
+  <br>
 <!--  <button v-on:click="toggleStatus">Change Status</button>-->
   <button @click="toggleStatus">Change Status</button>
 </template>
