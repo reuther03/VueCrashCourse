8931d0e2ec66f41be8945db5bd989f9744d8f477
michaldrozd <michaldrozd03@gmail.com>
2024-07-29 02:03:46 +0200

    Added job cards

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -3,6 +3,14 @@ import jobsData from '@/jobs.json'
 import {ref} from 'vue'
 import JobListing from "@/components/JobListing.vue";
 
+defineProps({
+  limit: Number,
+  showButton: {
+    type: Boolean,
+    default: false
+  }
+})
+
 const jobs = ref(jobsData)
 </script>
 
@@ -13,9 +21,17 @@ const jobs = ref(jobsData)
         Browse Jobs
       </h2>
       <div class="grid gird-cols-1 md:grid-cols-3 gap-6">
-        <JobListing v-for="job in jobs" :key="job.id" :job="job" />
+        <JobListing v-for="job in jobs.slice(0, limit||jobs.lenght)" :key="job.id" :job="job" />
       </div>
     </div>
+  </section>
+
+  <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+    <a
+        href="/jobs"
+        class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+    >View All Jobs</a
+    >
   </section>
 
 </template>
