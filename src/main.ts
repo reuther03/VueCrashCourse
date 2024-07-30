ab5e4085db79cf36f4f199ad5a630a403615818d
michaldrozd <michaldrozd03@gmail.com>
2024-07-30 18:21:09 +0200

    Added router

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import './assets/main.css'
+import 'primeicons/primeicons.css'
+
 
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router'
 
 const app = createApp(App)
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,16 @@
-<script setup lang="ts">
+﻿<script setup lang="ts">
+
+import HomeCards from "@/components/HomeCards.vue";
+import Hero from "@/components/Hero.vue";
+import JobListings from "@/components/JobListings.vue";
 </script>
 
 <template>
+  <Hero />
+  <HomeCards />
+  <JobListings :limit="3" :show-button="true"/>
 </template>
+
+<style scoped>
+
+</style>
